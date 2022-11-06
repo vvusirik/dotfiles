6845ad6d78de63d48c587fe585477d8977fa5308
Vishal Vusirikala <vvusirik@gmail.com>
2022-11-06 16:59:00 -0500

    include worktree context in picker

--- nvim/.config/nvim/lua/custom_pickers.lua
+++ nvim/.config/nvim/lua/custom_pickers.lua
@@ -1,6 +1,9 @@
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
+local putils = require "telescope.previewers.utils"
+local make_entry = require "telescope.make_entry"
+local previewers = require "telescope.previewers"
 local utils = require "telescope.utils"
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
@@ -12,24 +15,28 @@ custom_pickers.worktrees = function(opts)
 
     -- Populate worktree list
     local out, _, _ = utils.get_os_command_output({"git", "worktree", "list"}, opts.cwd)
+
     local worktree_list = {}
     for key, value in pairs(out) do
-        worktree_list[key] = vim.split(value, " ")[1]
+        worktree_list[key] = value
     end
 
     pickers.new(
         opts,
         {
             prompt_title = "Git Worktrees",
-            finder = finders.new_table {results = worktree_list},
+            finder = finders.new_table { results = worktree_list },
             sorter = conf.generic_sorter(opts),
             attach_mappings = function(prompt_bufnr, map)
                 actions.select_default:replace(
                     function()
                         actions.close(prompt_bufnr)
                         local selection = action_state.get_selected_entry()
-                        vim.cmd("cd " .. selection[1])
-                        print("Changed worktree to " .. selection[1])
+                        local dir = vim.split(selection[1], " ")[1]
+                        -- Change into worktree directory
+                        vim.cmd("cd " .. dir)
+                        print("Changed worktree to " .. dir)
+                        -- set a blank buffer in the new workspace
                         vim.cmd("enew")
                     end
                 )
@@ -39,5 +46,7 @@ custom_pickers.worktrees = function(opts)
         }
     ):find()
 end
+
+custom_pickers.worktrees()
 
 return custom_pickers
