ae65871e824170e63b1107646adbeee3727c4e4b
Vishal Vusirikala <vvusirik@gmail.com>
2025-07-10 10:08:03 -0400

    add diary keymap

--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,17 +1,17 @@
 return {
-    "vimwiki/vimwiki",
-    name = "vimwiki",
-    event = "BufEnter *.md",
-    keys = { "<leader>ww", "<leader>wt" },
-    init = function()
-        vim.g.vimwiki_list = {
-            {
-                path = '~/vimwiki/',
-                auto_diary_index = 1,
-                syntax = 'markdown',
-                ext = 'md'
-            }
-        }
-        vim.g.vimwiki_listsyms = '✗○◐●✓'
-    end,
+	"vimwiki/vimwiki",
+	name = "vimwiki",
+	event = "BufEnter *.md",
+	keys = { "<leader>ww", "<leader>wt", "<leader>w<leader>w" },
+	init = function()
+		vim.g.vimwiki_list = {
+			{
+				path = "~/wiki/",
+				auto_diary_index = 1,
+				syntax = "markdown",
+				ext = "md",
+			},
+		}
+		vim.g.vimwiki_listsyms = "✗○◐●✓"
+	end,
 }
